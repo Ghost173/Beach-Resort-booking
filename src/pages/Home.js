bf155e990d0148a8bb8bd26cff603e862ba16101
ghost <jpirakas007@gmail.com>
2019-06-19 10:03:55 +0530

    homaepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,8 @@
 
-import React from 'react'
-
+import React from 'react';
+import Hero from '../components/Hero';
 export default function Home() {
     return (
-        <div>
-            hello from home js
-        </div>
+        <Hero />
     )
 }
